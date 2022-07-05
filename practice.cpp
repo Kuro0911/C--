d93e777a306b5204a37f4aaacc3579907e301323
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-07-06 03:25:02 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -1,5 +1,3 @@
-/*
-// Q1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,132 +11,60 @@ typedef pair<pii, int> ppi;
 typedef pair<int, pii> pip;
 typedef pair<pii, pii> ppp;
 
-void getMulti(int n)
+void dfs(int r, int c, vector<vector<int>> &vis, int prevHeight, vector<vector<int>> &heights)
 {
-    for (int i = 1; i < n; i++)
-    {
-        if (i % 3 == 0)
-        {
-            cout << i << " ";
-        }
-    }
-    cout << endl;
-}
-void RGetMulti(int n, int i)
-{
-    if (i > n)
+    if (vis[r][c] == 1 || r < 0 || c < 0 || r == heights[0].size() - 1 || c == heights.size() - 1 || heights[r][c] < prevHeight)
     {
         return;
     }
-    if (i % 3 == 0)
-    {
-        cout << i << " ";
-    }
-    i++;
-    RGetMulti(n, i);
+    vis[r][c] = 1;
+    dfs(r + 1, c, vis, heights[r][c], heights);
+    dfs(r - 1, c, vis, heights[r][c], heights);
+    dfs(r, c + 1, vis, heights[r][c], heights);
+    dfs(r, c - 1, vis, heights[r][c], heights);
+    return;
 }
+vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights)
+{
+    int cols = heights.size() - 1, rows = heights[0].size() - 1;
+    vector<vector<int>> atl(cols + 1, vector<int>(rows + 1, 0)), pac(cols + 1, vector<int>(rows + 1, 0));
+    dfs(0, 1, pac, heights[0][1], heights);
+    for (int i = 0; i < cols; i++)
+    {
+        dfs(0, i, pac, heights[0][i], heights);
+        dfs(rows, i, atl, heights[rows][i], heights);
+    }
+    for (int i = 0; i < rows; i++)
+    {
+        dfs(0, i, pac, heights[i][0], heights);
+        dfs(rows, i, atl, heights[i][cols], heights);
+    }
+    vector<vector<int>> res{{1, 2}};
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; i++)
+        {
+            if (pac[i][j] == 0 && atl[i][j] == 0)
+            {
+                res.push_back({i, j});
+            }
+        }
+    }
+    return res;
+}
+
 void solve()
 {
-    int n;
-    cin >> n;
-    getMulti(n);
-    RGetMulti(n, 1);
-    cout << endl;
-}
-
-signed main()
-{
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t = 1;
-    // cin>>t;
-    while (t--)
+    vector<vector<int>> heights{{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
+    vector<vector<int>> res = pacificAtlantic(heights);
+    for (auto &x : res)
     {
-        solve();
+        for (auto &y : x)
+        {
+            cout << y << " ";
+        }
+        cout << endl;
     }
-#ifndef ONLINE_JUDGE
-    cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
-#endif
-    return 0;
-}
-*/
-
-// Q2
-#include <bits/stdc++.h>
-using namespace std;
-
-#define int long long
-#define tb ' '
-#define all(a) (a).begin(), (a).end()
-#define sz(x) ((int)x.size())
-#define MOD (int)(1e9 + 7)
-typedef pair<int, int> pii;
-typedef pair<pii, int> ppi;
-typedef pair<int, pii> pip;
-typedef pair<pii, pii> ppp;
-
-void TwoSum(vector<int> &vec, int target)
-{
-    map<int, int> mp;
-    for (auto &x : vec)
-    {
-        int fndTar = x - target;
-        for (int i = 0; i < vec.size(); i++)
-        {
-            int tar = target - vec[i];
-            if (mp.find(tar) != mp.end())
-            {
-                cout << i + 1 << " " << mp[tar] + 1;
-                return;
-            }
-            mp[vec[i]] = i;
-        }
-    }
-}
-void AllSum(vector<int> &vec, int target)
-{
-    int st = 0, end = vec.size() - 1;
-    while (st < end)
-    {
-        int crrSum = 0;
-        for (int i = st; i <= end; i++)
-        {
-            crrSum += vec[i];
-        }
-        if (crrSum == target)
-        {
-            for (int i = st; i <= end; i++)
-            {
-                cout << i + 1 << " ";
-            }
-            cout << endl;
-            return;
-        }
-        if (crrSum < target)
-        {
-            st++;
-            end = vec.size() - 1;
-        }
-        if (crrSum > target)
-        {
-            end--;
-        }
-    }
-}
-void solve()
-{
-    int n, target;
-    cin >> target >> n;
-    vector<int> vec(n);
-    for (auto &x : vec)
-    {
-        cin >> x;
-    }
-    // TwoSum(vec, target);
-    AllSum(vec, target);
 }
 
 signed main()
