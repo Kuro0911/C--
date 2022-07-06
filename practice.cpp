ddb91d8091c9a62daec708fd1cad02a357cd52c0
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-07-06 15:35:53 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -11,60 +11,8 @@ typedef pair<pii, int> ppi;
 typedef pair<int, pii> pip;
 typedef pair<pii, pii> ppp;
 
-void dfs(int r, int c, vector<vector<int>> &vis, int prevHeight, vector<vector<int>> &heights)
-{
-    if (vis[r][c] == 1 || r < 0 || c < 0 || r == heights[0].size() - 1 || c == heights.size() - 1 || heights[r][c] < prevHeight)
-    {
-        return;
-    }
-    vis[r][c] = 1;
-    dfs(r + 1, c, vis, heights[r][c], heights);
-    dfs(r - 1, c, vis, heights[r][c], heights);
-    dfs(r, c + 1, vis, heights[r][c], heights);
-    dfs(r, c - 1, vis, heights[r][c], heights);
-    return;
-}
-vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights)
-{
-    int cols = heights.size() - 1, rows = heights[0].size() - 1;
-    vector<vector<int>> atl(cols + 1, vector<int>(rows + 1, 0)), pac(cols + 1, vector<int>(rows + 1, 0));
-    dfs(0, 1, pac, heights[0][1], heights);
-    for (int i = 0; i < cols; i++)
-    {
-        dfs(0, i, pac, heights[0][i], heights);
-        dfs(rows, i, atl, heights[rows][i], heights);
-    }
-    for (int i = 0; i < rows; i++)
-    {
-        dfs(0, i, pac, heights[i][0], heights);
-        dfs(rows, i, atl, heights[i][cols], heights);
-    }
-    vector<vector<int>> res{{1, 2}};
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; i++)
-        {
-            if (pac[i][j] == 0 && atl[i][j] == 0)
-            {
-                res.push_back({i, j});
-            }
-        }
-    }
-    return res;
-}
-
 void solve()
 {
-    vector<vector<int>> heights{{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
-    vector<vector<int>> res = pacificAtlantic(heights);
-    for (auto &x : res)
-    {
-        for (auto &y : x)
-        {
-            cout << y << " ";
-        }
-        cout << endl;
-    }
 }
 
 signed main()
