552a7387f5778233b43d2dc9f3aeff17e399bb72
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-08-01 22:27:47 +0530

    init

--- algo/reversort.cpp
+++ algo/reversort.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -73,6 +74,54 @@ signed main()
     // {
     //     solve();
     // }
+#ifndef ONLINE_JUDGE
+    cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
+#endif
+    return 0;
+}
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define tb ' '
+#define all(a) (a).begin(), (a).end()
+#define sz(x) ((int)x.size())
+#define MOD (int)(1e9 + 7)
+typedef pair<int, int> pii;
+typedef pair<pii, int> ppi;
+typedef pair<int, pii> pip;
+typedef pair<pii, pii> ppp;
+
+void solve()
+{
+    int n, c;
+    cin >> n >> c;
+    if (c < n - 1 || c > (n * (n - 1) / 2) - 1)
+    {
+        cout << "IMPOSSIBLE\n";
+        return;
+    }
+}
+
+signed main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    // while (t--)
+    // {
+    //     solve();
+    // }
+    for (int i = 1; i <= t; i++)
+    {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
 #ifndef ONLINE_JUDGE
     cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
 #endif
