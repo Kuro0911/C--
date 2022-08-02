12429e0e4aaf1978cec9c002e1d0e6a0908ce179
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-08-02 09:58:40 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -11,48 +11,8 @@ typedef pair<pii, int> ppi;
 typedef pair<int, pii> pip;
 typedef pair<pii, pii> ppp;
 
-int getMin(vector<int> vec, int st, int n)
-{
-    int minInd = st;
-    int mn = INT_MAX;
-    for (int i = st; i < n; i++)
-    {
-        if (vec[i] < mn)
-        {
-            mn = vec[i];
-            minInd = i;
-        }
-    }
-    return minInd;
-}
-void reverSort(vector<int> &vec, int n)
-{
-    int cost = 0;
-    int st = 0;
-    while (st < n)
-    {
-        int ed = getMin(vec, st, n);
-        reverse(vec.begin() + st, vec.begin() + ed + 1);
-        cost += distance(vec.begin() + st, vec.begin() + ed + 1);
-        st++;
-    }
-    cout << cost - 1 << "\n";
-}
-
 void solve()
 {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (auto &x : vec)
-    {
-        cin >> x;
-    }
-    reverSort(vec, n);
-    // for (auto x : vec)
-    // {
-    //     cout << x << " ";
-    // }
 }
 
 signed main()
@@ -62,17 +22,17 @@ signed main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int t;
-    cin >> t;
-    for (int i = 1; i <= t; i++)
+    int t = 1;
+    // cin>>t;
+    while (t--)
     {
-        cout << "Case #" << i << ": ";
         solve();
     }
-    // while (t--)
-    // {
-    //     solve();
-    // }
+// for (int i = 1; i <= t; i++)
+//{
+// cout << "Case #" << i << ": " ;
+// solve();
+//}
 #ifndef ONLINE_JUDGE
     cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
 #endif
