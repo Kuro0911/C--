0f0168ff55b775127fdd110e78bd0302b3387677
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-08-02 13:14:15 +0530

    dp init

--- practice.cpp
+++ practice.cpp
@@ -1,3 +1,11 @@
+/*
+    We create a 2D vector containing "n"
+    elements each having the value "vector<int> (m, 0)".
+    "vector<int> (m, 0)" means a vector having "m"
+    elements each of value "0".
+    Here these elements are vectors.
+    //vector<vector<int>> vec(n, vector<int>(m, 0));
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,8 +19,28 @@ typedef pair<pii, int> ppi;
 typedef pair<int, pii> pip;
 typedef pair<pii, pii> ppp;
 
+// knapsack
 void solve()
 {
+    vector<int> wt{1, 2, 3, 5};
+    vector<int> prof{5, 4, 3, 1};
+    int W = 5;
+    vector<vector<int>> dp(wt.size(), vector<int>(W + 1, 0));
+    for (int i = 1; i < wt.size(); i++)
+    {
+        for (int j = 1; j < W + 1; j++)
+        {
+            if (wt[i - 1] <= j)
+            {
+                dp[i][j] = max(prof[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    cout << dp[wt.size() - 1][W];
 }
 
 signed main()
