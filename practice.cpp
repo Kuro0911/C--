56e23b9c7c08b1a031f21d6f9ab071bfd4818072
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-08-04 12:26:53 +0530

    print ssc

--- practice.cpp
+++ practice.cpp
@@ -1,133 +1,67 @@
-/*
-    We create a 2D vector containing "n"
-    elements each having the value "vector<int> (m, 0)".
-    "vector<int> (m, 0)" means a vector having "m"
-    elements each of value "0".
-    Here these elements are vectors.
-    //vector<vector<int>> vec(n, vector<int>(m, 0));
-*/
-#include <bits/stdc++.h>
-using namespace std;
-
-#define int long long
-#define tb ' '
-#define all(a) (a).begin(), (a).end()
-#define sz(x) ((int)x.size())
-#define MOD (int)(1e9 + 7)
-typedef pair<int, int> pii;
-typedef pair<pii, int> ppi;
-typedef pair<int, pii> pip;
-typedef pair<pii, pii> ppp;
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &v)
+class Solution
 {
-    for (auto &x : v)
-        os << x << " ";
-    os << endl;
-    return os;
-}
-template <typename T>
-
-ostream &operator<<(ostream &os, const set<T> &v)
-{
-    for (auto it : v)
-        os << it << " ";
-    return os;
-}
-
-template <typename T, typename S>
-ostream &operator<<(ostream &os, const map<T, S> &v)
-{
-    for (auto it : v)
-        os << it.first << " : " << it.second << "\n";
-
-    return os;
-}
-template <typename T, typename S>
-ostream &operator<<(ostream &os, const pair<T, S> &v)
-{
-    os << v.first << " : " << v.second << "\n";
-    return os;
-}
-
-void solve()
-{
-    string s1, s2;
-    cin >> s1 >> s2;
-    int n = s1.size(), m = s2.size();
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-    pair<int, pair<int, int>> mx = {-1, {-1, -1}};
-    for (int i = 0; i <= n; i++)
+public:
+    string shortestCommonSupersequence(string s1, string s2)
     {
-        dp[i][0] = 0;
-    }
-    for (int i = 0; i <= m; i++)
-    {
-        dp[0][i] = 0;
-    }
-
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        for (int i = 0; i <= n; i++)
         {
-            if (s1[i - 1] == s2[j - 1])
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++)
+        {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
             {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-                if (mx.first < dp[i][j])
+                if (s1[i - 1] == s2[j - 1])
                 {
-                    mx.first = dp[i][j];
-                    mx.second.first = i;
-                    mx.second.second = j;
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        string res;
+        int x = n, y = m;
+        while (x > 0 && y > 0)
+        {
+            if (s1[x - 1] == s2[y - 1])
+            {
+                res.push_back(s1[x - 1]);
+                x--;
+                y--;
             }
             else
             {
-                dp[i][j] = 0;
+                if (dp[x - 1][y] > dp[x][y - 1])
+                {
+                    res.push_back(s1[x - 1]);
+                    x--;
+                }
+                else
+                {
+                    res.push_back(s2[y - 1]);
+                    y--;
+                }
             }
         }
-    }
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= m; j++)
+        while (x > 0)
         {
-            cout << dp[i][j] << " ";
+            res.push_back(s1[x - 1]);
+            x--;
         }
-        cout << endl;
-    }
-    int x = mx.second.first, y = mx.second.second;
-    string res;
-    while (x >= 0 || y >= 0)
-    {
-        if (dp[x][y] == 0)
+        while (y > 0)
         {
-            break;
+            res.push_back(s2[y - 1]);
+            y--;
         }
-        res += s1[x];
-        x--;
-        y--;
+        reverse(res.begin(), res.end());
+        return res;
     }
-    cout << res;
-}
-signed main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t = 1;
-    // cin>>t;
-    while (t--)
-    {
-        solve();
-    }
-// for (int i = 1; i <= t; i++)
-//{
-// cout << "Case #" << i << ": " ;
-// solve();
-//}
-#ifndef ONLINE_JUDGE
-    cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
-#endif
-    return 0;
-}
+};
