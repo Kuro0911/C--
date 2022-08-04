790c41b5797b4e7f76e927d5540f41e15e60a90d
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-08-04 16:44:20 +0530

    mcm

--- practice.cpp
+++ practice.cpp
@@ -1,67 +1,97 @@
-class Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define tb ' '
+#define all(a) (a).begin(), (a).end()
+#define sz(x) ((int)x.size())
+#define MOD (int)(1e9 + 7)
+typedef pair<int, int> pii;
+typedef pair<pii, int> ppi;
+typedef pair<int, pii> pip;
+typedef pair<pii, pii> ppp;
+
+//#####################################################
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
 {
-public:
-    string shortestCommonSupersequence(string s1, string s2)
+    for (auto &x : v)
+        os << x << " ";
+    os << endl;
+    return os;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &v)
+{
+    for (auto it : v)
+        os << it << " ";
+    return os;
+}
+template <typename T, typename S>
+ostream &operator<<(ostream &os, const map<T, S> &v)
+{
+    for (auto it : v)
+        os << it.first << " : " << it.second << endl;
+    return os;
+}
+template <typename T, typename S>
+ostream &operator<<(ostream &os, const pair<T, S> &v)
+{
+    os << v.first << " : " << v.second << endl;
+    return os;
+}
+
+//#####################################################
+
+int mcm(vector<int> &arr, int i, int j, vector<vector<int>> &dp)
+{
+    if (dp[i][j] != -1)
+        return dp[i][j];
+    int ans = INT_MAX;
+    if (i >= j)
+        return 0;
+    for (int k = i; k <= j - 1; k++)
     {
-        int n = s1.size(), m = s2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        for (int i = 0; i <= n; i++)
-        {
-            dp[i][0] = 0;
-        }
-        for (int i = 0; i <= m; i++)
-        {
-            dp[0][i] = 0;
-        }
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                if (s1[i - 1] == s2[j - 1])
-                {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                else
-                {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        string res;
-        int x = n, y = m;
-        while (x > 0 && y > 0)
-        {
-            if (s1[x - 1] == s2[y - 1])
-            {
-                res.push_back(s1[x - 1]);
-                x--;
-                y--;
-            }
-            else
-            {
-                if (dp[x - 1][y] > dp[x][y - 1])
-                {
-                    res.push_back(s1[x - 1]);
-                    x--;
-                }
-                else
-                {
-                    res.push_back(s2[y - 1]);
-                    y--;
-                }
-            }
-        }
-        while (x > 0)
-        {
-            res.push_back(s1[x - 1]);
-            x--;
-        }
-        while (y > 0)
-        {
-            res.push_back(s2[y - 1]);
-            y--;
-        }
-        reverse(res.begin(), res.end());
-        return res;
+        int temp = mcm(arr, i, k, dp) + mcm(arr, k + 1, j, dp) + arr[i - 1] * arr[k] * arr[j];
+        ans = min(temp, ans);
     }
-};
+    return dp[i][j] = ans;
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+    for (auto &x : arr)
+    {
+        cin >> x;
+    }
+
+    cout << mcm(arr, 1, n - 1, dp);
+}
+
+signed main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+// for (int i = 1; i <= t; i++)
+//{
+// cout << "Case #" << i << ": " ;
+// solve();
+//}
+#ifndef ONLINE_JUDGE
+    cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
+#endif
+    return 0;
+}
