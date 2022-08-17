ad065b8fef0ee36dfaf97fcad079ada5db6bb5ce
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-08-17 20:01:35 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -43,74 +43,9 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 }
 
 //#####################################################
-set<pair<int, int>> vis;
-void dfs(int i, int j, vector<vector<char>> &board, set<pair<int, int>> &path)
-{
-    if (path.empty())
-    {
-        cout << i << " " << j << endl;
-        return;
-    }
-    if (path.find({i, j}) != path.end() && path.size() != 1)
-    {
-        return;
-    }
-    if (i < board.size() && i >= 0 && j < board[i].size() && j >= 0)
-    {
-        if (board[i][j] == 'O')
-        {
-            vis.insert({i, j});
-            if (i == 0 || i == board.size() - 1 || j == 0 || j == board[i].size() - 1)
-            {
-                path.clear();
-                return;
-            }
-            path.insert({i, j});
-            dfs(i + 1, j, board, path);
-            dfs(i - 1, j, board, path);
-            dfs(i, j + 1, board, path);
-            dfs(i, j - 1, board, path);
-            return;
-        }
-    }
-    return;
-};
-void solve1(vector<vector<char>> &board)
-{
-    for (int i = 1; i < board.size() - 1; i++)
-    {
-        for (int j = 1; j < board[i].size() - 1; j++)
-        {
-            if (board[i][j] == 'O' && vis.find({i, j}) == vis.end())
-            {
-                set<pair<int, int>> path;
-                path.insert({i, j});
-                dfs(i, j, board, path);
-                for (auto x : path)
-                {
-                    cout << x;
-                }
 
-                if (!path.empty())
-                {
-                    for (auto x : path)
-                    {
-                        board[x.first][x.second] = 'X';
-                    }
-                }
-            }
-        }
-    }
-}
 void solve()
 {
-    vector<vector<char>> board{
-        {'O', 'O', 'O', 'O', 'X', 'X'}, {'O', 'O', 'O', 'O', 'O', 'O'}, {'O', 'X', 'O', 'X', 'O', 'O'}, {'O', 'X', 'O', 'O', 'X', 'O'}, {'O', 'X', 'O', 'X', 'O', 'O'}, {'O', 'X', 'O', 'O', 'O', 'O'}};
-    solve1(board);
-    for (auto x : board)
-    {
-        cout << x;
-    }
 }
 
 signed main()
@@ -121,7 +56,7 @@ signed main()
     cout.tie(NULL);
 
     int t = 1;
-    // cin >> t;
+    // cin>>t;
     while (t--)
     {
         solve();
