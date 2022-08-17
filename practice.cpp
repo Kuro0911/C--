19601aa725b40b3913ec287af87e8a748dd6a897
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-08-17 23:30:50 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -43,9 +43,46 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 }
 
 //#####################################################
-
+int getCost(int st, vector<int> gas, vector<int> cost)
+{
+    int sz = gas.size();
+    for (int i = 0; i < sz; i++)
+    {
+        gas.push_back(gas[i]);
+    }
+    int g = 0;
+    cout << " st : " << st << endl;
+    for (int i = st; i < st + sz; i++)
+    {
+        cout << g << "\n";
+        g += gas[st];
+        g -= cost[abs(sz - i) + 1];
+        if (g < 0)
+            return -1;
+    }
+    return st;
+}
+int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+{
+    map<int, int> mp;
+    for (int i = 0; i < gas.size(); i++)
+    {
+        mp[-gas[i]] = i;
+    }
+    int ans = -1;
+    for (auto x : mp)
+    {
+        ans = getCost(x.second, gas, cost);
+        if (ans != -1)
+            return x.second;
+    }
+    return ans;
+}
 void solve()
 {
+    vector<int> gas{1, 2, 3, 4, 5};
+    vector<int> cost{3, 4, 5, 1, 2};
+    cout << canCompleteCircuit(gas, cost);
 }
 
 signed main()
