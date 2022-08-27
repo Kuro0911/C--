54c1d7302b16c9641044abd7304064e1b4e72abc
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-08-27 20:36:40 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -44,11 +44,37 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 
 //#####################################################
 
+int minimumLoss(vector<int> price)
+{
+    int minloss = INT_MIN;
+    int size = price.size();
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (price.at(j) - price.at(i) < 0)
+            {
+                if (price.at(j) - price.at(i) > minloss)
+                {
+                    minloss = price.at(j) - price.at(i);
+                }
+            }
+        }
+    }
+    return 0 - minloss;
+}
 void solve()
 {
-    string s = "ABCD";
-    cout << s.substr(1) << " " << s.substr(0, s.size() - 1);
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (auto &x : vec)
+    {
+        cin >> x;
+    }
+    cout << minimumLoss(vec) << endl;
 }
+
 signed main()
 {
 
@@ -57,7 +83,7 @@ signed main()
     cout.tie(NULL);
 
     int t = 1;
-    // cin>>t;
+    cin >> t;
     while (t--)
     {
         solve();
