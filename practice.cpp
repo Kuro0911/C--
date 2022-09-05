3a703664f776946644624ec4971bbd4755d1ab28
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-09-05 13:18:40 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -44,11 +44,43 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 
 //#####################################################
 
+void helper(int i, int j, int n, vector<vector<int>> &ans, vector<pair<int, int>> dir, int &d)
+{
+    if (i >= ans.size() && i < 0 && j >= ans[i].size() || j < 0 || ans[i][j] != -1)
+    {
+        return;
+    }
+    ans[i][j] = n;
+    int x, y;
+    while (d < 4)
+    {
+        x = i + dir[d].first;
+        y = j + dir[d].second;
+        if (x < ans.size() && x >= 0 && y < ans[i].size() && y >= 0 && ans[x][y] == -1)
+        {
+            helper(x, y, n + 1, ans, dir, d);
+            return;
+        }
+        d++;
+    }
+    d = 0;
+    x = i + dir[0].first;
+    y = j + dir[0].second;
+    helper(x, y, n + 1, ans, dir, d);
+}
+
 void solve()
 {
-    char x = 1 + '0';
-    int y = x - '0';
-    cout << y;
+    int n;
+    cin >> n;
+    vector<vector<int>> ans(n, vector<int>(n, -1));
+    vector<pair<int, int>> dir{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int d = 0;
+    helper(0, 0, 1, ans, dir, d);
+    for (auto x : ans)
+    {
+        cout << x;
+    }
 }
 
 signed main()
