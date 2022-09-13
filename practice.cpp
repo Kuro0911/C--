14a3340fdc25ba939d6eb7843133b431df80312a
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-09-13 07:35:19 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -43,54 +43,9 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 }
 
 //#####################################################
-int kthSmallest(vector<vector<int>> &mat, int k)
-{
-    priority_queue<vector<int>> max_heap;
-    set<vector<int>> vis;
-    int n = mat.size(), m = mat[0].size();
-    int s = 0;
-    for (int i = 0; i < n; i++)
-        s += mat[i][0];
-    vector<int> temp{s};
-    for (int i = 0; i < n; i++)
-        temp.push_back(0);
-    max_heap.push(temp);
-    while (k > 1)
-    {
-        vector<int> prev = max_heap.top();
-        max_heap.pop();
-        k--;
-        for (int i = 0; i < n; i++)
-        {
-            if (prev[i + 1] == m - 1)
-            {
-                continue;
-            }
-            vector<int> next = prev;
-            next[0] -= mat[i][next[i + 1]];
-            next[i + 1] += 1;
-            next[0] += mat[i][next[i + 1]];
-            cout << next;
-            if (vis.find(next) != vis.end())
-            {
-                cout << next;
-                continue;
-            }
-            max_heap.push(next);
-            vis.insert(next);
-        }
-    }
-    // while (!max_heap.empty())
-    // {
-    //     cout << max_heap.top();
-    //     max_heap.pop();
-    // }
-    return -1;
-}
+
 void solve()
 {
-    vector<vector<int>> vec{{1, 3, 11}, {2, 4, 6}};
-    cout << kthSmallest(vec, 5);
 }
 
 signed main()
