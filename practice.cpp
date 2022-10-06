e217fa9a31e5d7bef84542dfee676d92940da90e
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-10-06 12:57:02 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -44,98 +44,16 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 
 //#####################################################
 
-class DSU
-{
-public:
-    map<pair<int, int>, pair<int, int>> parent;
-
-    void make(int i, int j)
-    {
-        parent[{i, j}] = {i, j};
-    };
-
-    pair<int, int> find(pair<int, int> x)
-    {
-        if (x != parent[x])
-            parent[x] = find(parent[x]);
-        return parent[x];
-    };
-
-    void Union(pair<int, int> a, pair<int, int> b)
-    {
-        a = find(a);
-        b = find(b);
-        parent[a] = b;
-    }
-};
-class Solution
-{
-public:
-    int latestDayToCross(int row, int col, vector<vector<int>> &cells)
-    {
-        DSU node;
-        int ans = 0;
-
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < col; j++)
-            {
-                node.parent[{i, j}] = {i, j};
-            }
-        }
-        pair<int, int> l = {-1, -1}, r = {-2, -2};
-
-        node.parent[l] = l;
-        node.parent[r] = r;
-
-        vector<pair<int, int>> dir{{1, 0}, {1, 1}, {1, -1}, {0, -1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}};
-        set<pair<int, int>> vis;
-
-        for (auto &c : cells)
-        {
-            int x = c[0] - 1, y = c[1] - 1;
-            for (auto d : dir)
-            {
-                int nx = x + d.first, ny = y + d.second;
-                if (nx >= 0 and nx < row)
-                {
-                    if (ny >= 0 and ny < col)
-                    {
-                        if (vis.find({nx, ny}) != vis.end())
-                        {
-                            node.Union({x, y}, {nx, ny});
-                        }
-                    }
-                    else
-                    {
-                        if (ny == -1)
-                        {
-                            node.Union({x, y}, l);
-                        }
-                        if (ny == col)
-                        {
-                            node.Union({x, y}, r);
-                        }
-                    }
-                }
-                pair<int, int> tempL = node.find(l), tempR = node.find(r);
-                if (tempL == tempR)
-                {
-                    return ans;
-                };
-                vis.insert({x, y});
-            }
-            ans++;
-        }
-        return ans;
-    }
-};
 void solve()
 {
-    Solution x;
-    vector<vector<int>> vec{
-        {1, 1}, {2, 1}, {1, 2}, {2, 2}};
-    cout << x.latestDayToCross(2, 2, vec);
+    map<int, int> mp;
+    mp[1] = 0;
+    mp[2] = 0;
+    if (mp[1] == 0)
+    {
+        mp.erase(1);
+    }
+    cout << mp;
 }
 
 signed main()
