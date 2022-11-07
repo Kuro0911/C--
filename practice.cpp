6a1ba33cdbff64a2d7beaca5517ba3c7aefcacef
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-11-07 19:26:36 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -43,10 +43,44 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 }
 
 //#####################################################
+class Solution
+{
+public:
+    string originalDigits(string s)
+    {
+        vector<int> num(10, 0);
+        vector<int> count(26, 0);
 
+        for (auto c : s)
+            count[c - 'a']++;
+
+        num[0] = count['z' - 'a'];
+        num[2] = count['w' - 'a'];
+        num[4] = count['u' - 'a'];
+        num[6] = count['x' - 'a'];
+        num[8] = count['g' - 'a'];
+
+        num[1] = count['o' - 'a'] - num[0] - num[2] - num[4];
+        num[3] = count['h' - 'a'] - num[8];
+        num[5] = count['f' - 'a'] - num[4];
+        num[7] = count['s' - 'a'] - num[6];
+        num[9] = count['i' - 'a'] - num[5] - num[6] - num[8];
+
+        string res;
+        for (int i = 0; i < 10; i++)
+        {
+            int c = num[i];
+            while (c > 0)
+            {
+                res += to_string(i);
+                c--;
+            }
+        }
+        return res;
+    }
+};
 void solve()
 {
-    cout << 8.67 + 8.40;
 }
 
 signed main()
@@ -72,11 +106,3 @@ signed main()
 #endif
     return 0;
 }
-
-/*
-
-FORMAL LANGUAGE AND AUTOMATA 	6.00 / 10.00
-COMPUTER NETWORKS 	4.33 / 5.00         37 / 50
-DISCRETE MATHEMATICS FOR ENGINEERS    	8.67 / 10.00     8.40 / 15.00 =>   / 25 !!!
-
-*/
