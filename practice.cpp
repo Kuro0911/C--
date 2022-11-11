71799e48f163152287d7a7061306b4f30e6f9881
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-11-11 15:20:24 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -1,156 +1,110 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <string>
-#include <algorithm>
-#include <set>
-#include <cassert>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node
+#define int long long
+#define tb ' '
+#define all(a) (a).begin(), (a).end()
+#define sz(x) ((int)x.size())
+#define MOD (int)(1e9 + 7)
+typedef pair<int, int> pii;
+typedef pair<pii, int> ppi;
+typedef pair<int, pii> pip;
+typedef pair<pii, pii> ppp;
+
+//#####################################################
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
 {
-    Node *next;
-    Node *prev;
-    int value;
-    int key;
-    Node(Node *p, Node *n, int k, int val) : prev(p), next(n), key(k), value(val){};
-    Node(int k, int val) : prev(NULL), next(NULL), key(k), value(val){};
-};
-
-class Cache
+    for (auto &x : v)
+        os << x << " ";
+    os << endl;
+    return os;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &v)
 {
+    for (auto it : v)
+        os << it << " ";
+    return os;
+}
+template <typename T, typename S>
+ostream &operator<<(ostream &os, const map<T, S> &v)
+{
+    for (auto it : v)
+        os << it.first << " : " << it.second << endl;
+    return os;
+}
+template <typename T, typename S>
+ostream &operator<<(ostream &os, const pair<T, S> &v)
+{
+    os << v.first << " : " << v.second << endl;
+    return os;
+}
 
-protected:
-    map<int, Node *> mp;            // map the key to the node in the linked list
-    int cp;                         // capacity
-    Node *tail;                     // double linked list tail pointer
-    Node *head;                     // double linked list head pointer
-    virtual void set(int, int) = 0; // set function
-    virtual int get(int) = 0;       // get function
-};
-
-class LRUCache : public Cache
+//#####################################################
+class Solution
 {
 public:
-    LRUCache(int cap)
+    int helper(string ring, string key, int curr)
     {
-        cp = cap;
-        head = NULL;
-        tail = NULL;
+        if (key.size() == 0)
+        {
+            return 0;
+        }
+        curr %= ring.size();
+        if (ring[curr] == '#')
+        {
+            return INT_MAX;
+        }
+        int tot = 1;
+        if (ring[curr] == key[0])
+        {
+            tot++;
+            key = key.substr(1);
+        }
+        char x = ring[curr];
+        ring[curr] = '#';
+        int l = helper(ring, key, curr + 1);
+        int r = helper(ring, key, curr + ring.size() - 1);
+        ring[curr] = x;
+        tot += min(l, r);
+        return tot;
     }
-    void print()
+    int findRotateSteps(string ring, string key)
     {
-        Node *temp = head;
-        cout << "\nPRINT => \n";
-        cout << "HEAD : " << head->key << " , " << head->value << endl;
-        cout << "TAIL : " << tail->key << " , " << tail->value << endl;
-
-        while (temp)
-        {
-            cout << temp->key << " , " << temp->value << endl;
-            temp = temp->next;
-        }
-    }
-    void insert(int key, int val)
-    {
-        Node *new_node = new Node(key, val);
-        new_node->prev = NULL;
-        if (head)
-        {
-            mp[head->key]->prev = new_node;
-            new_node->next = mp[head->key];
-        }
-        else
-        {
-            tail = new_node;
-        }
-
-        mp[key] = new_node;
-        head = mp[key];
-    }
-    void pop(int key)
-    {
-        tail = mp[key]->prev;
-        mp[key]->prev->next = NULL;
-        mp.erase(key);
-    }
-    void move(int key)
-    {
-        if (head == mp[key])
-        {
-            return;
-        }
-        Node *prev = mp[key]->prev, *next = mp[key]->next;
-        if (prev)
-        {
-            prev->next = next;
-        }
-        if (next)
-        {
-            next->prev = prev;
-        }
-        if (tail == mp[key])
-        {
-            if (next)
-            {
-                tail = next;
-            }
-            else
-            {
-                tail = prev;
-            }
-        }
-        mp[key]->prev = NULL;
-        mp[key]->next = head;
-        head = mp[key];
-    }
-    virtual int get(int key)
-    {
-        if (mp.find(key) != mp.end())
-        {
-            return mp[key]->value;
-        }
-        return -1;
-    }
-    virtual void set(int key, int val)
-    {
-        if (mp.find(key) != mp.end())
-        {
-            mp[key]->value = val;
-            move(key);
-        }
-        else
-        {
-            int sz = mp.size();
-            if (sz == cp)
-            {
-                pop(tail->key);
-            }
-            insert(key, val);
-        }
+        int ans = helper(ring, key, 0);
+        return ans;
     }
 };
-int main()
+void solve()
 {
-    int n, capacity, i;
-    cin >> n >> capacity;
-    LRUCache l(capacity);
-    for (i = 0; i < n; i++)
+    Solution x;
+    string r, k;
+    cin >> r >> k;
+    cout << x.findRotateSteps(r, k);
+}
+
+signed main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    // cin>>t;
+    while (t--)
     {
-        string command;
-        cin >> command;
-        if (command == "get")
-        {
-            int key;
-            cin >> key;
-            cout << l.get(key) << endl;
-        }
-        else if (command == "set")
-        {
-            int key, value;
-            cin >> key >> value;
-            l.set(key, value);
-        }
+        solve();
     }
+// for (int i = 1; i <= t; i++)
+//{
+// cout << "Case #" << i << ": " ;
+// solve();
+//}
+#ifndef ONLINE_JUDGE
+    cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
+#endif
     return 0;
 }
