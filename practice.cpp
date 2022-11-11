d470d5efafdf16263c210981a116aa2c357b3ba3
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-11-11 15:44:14 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -46,37 +46,28 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 class Solution
 {
 public:
-    int helper(string ring, string key, int curr)
-    {
-        if (key.size() == 0)
-        {
-            return 0;
-        }
-        curr %= ring.size();
-
-        if (ring[curr] == '#')
-        {
-            return INT_MAX;
-        }
-        int tot = 0;
-        if (ring[curr] == key[0])
-        {
-            tot++;
-            key = key.substr(1);
-        }
-        char x = ring[curr];
-        ring[curr] = '#';
-        int l = helper(ring, key, curr + 1);
-        int r = helper(ring, key, curr + ring.size() - 1);
-        ring[curr] = x;
-        cout << l << " " << r << endl;
-        tot += min(l, r) + 1;
-        return tot;
-    }
     int findRotateSteps(string ring, string key)
     {
-        int ans = helper(ring, key, 0);
-        return ans;
+        vector<int> pos[26];
+        int r = ring.size();
+        for (int i = 0; i < r; i++)
+            pos[ring[i] - 'a'].push_back(i);
+        vector<vector<int>> mem(r, vector<int>(key.size()));
+        return findSteps(0, 0, ring, key, pos, mem);
+    }
+    int findSteps(int p1, int p2, string &ring, string &key, vector<int> pos[26], vector<vector<int>> &mem)
+    {
+        if (p2 == key.size())
+            return 0;
+        if (mem[p1][p2])
+            return mem[p1][p2];
+        int r = ring.size(), ms = INT_MAX;
+        for (int nxt : pos[key[p2] - 'a'])
+        {
+            int dist = abs(p1 - nxt);
+            ms = min(ms, min(dist, r - dist) + findSteps(nxt, p2 + 1, ring, key, pos, mem));
+        }
+        return mem[p1][p2] = ms + 1;
     }
 };
 void solve()
