b9e45e62cec26740e8311a6fdfa60b1999ee8c18
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-11-17 10:47:02 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -46,27 +46,31 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 class Solution
 {
 public:
-    int guessNumber(int n)
+    bool check(int x1, int y1, int x2, int y2)
     {
-        int low = 1, high = n;
-        while (low <= high)
+        if (x1 < x2 and y2 > y1)
         {
-            int md = low + (high - low) / 2;
-            int res = guess(md);
-            if (res == 0)
-            {
-                return md;
-            }
-            else if (res < 0)
-            {
-                high = md - 1;
-            }
-            else
-            {
-                low = md + 1;
-            }
+            return true;
         }
-        return -1;
+        return false;
+    }
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
+    {
+        int arrA = (ax2 - ax1) * (ay2 - ay1);
+        int arrB = (bx2 - bx1) * (by2 - by1);
+
+        int tot = arrA + arrB;
+
+        int cx = min(ax2, bx2) - max(ax1, bx1);
+        int cy = min(ay2, by2) - max(ay1, by1);
+
+        if (cx > 0 and cy > 0)
+        {
+            int arrC = cx * cy;
+            tot -= arrC;
+        }
+
+        return tot;
     }
 };
 void solve()
