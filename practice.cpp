ef7e8cb99dd5971cf61eaa456757b08f06b41a98
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-11-19 18:29:08 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -43,30 +43,40 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 }
 
 //#####################################################
-class Solution
+static vector<vector<int>> outerTrees(vector<vector<int>> &points)
 {
-public:
-    int helper(int n, int fact)
+    // Andrew's monotone chain method.
+    sort(begin(points), end(points), [](const vector<int> &a, const vector<int> &b)
+         { return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1]); });
+    // left to right
+    const int n = size(points);
+    vector<vector<int>> ans;
+    ans.reserve(n);
+    for (int i = 0; i < n; ++i)
     {
-        while (n % fact == 0)
-        {
-            n /= fact;
-        }
-        return n;
+        while (size(ans) > 1 && orientation(ans[size(ans) - 2], ans.back(), points[i]) < 0)
+            ans.pop_back();
+        ans.push_back(points[i]);
     }
-    bool isUgly(int n)
+    // If all points are along a line, size(ans) is n after left to right procedure.
+    if (size(ans) == n)
+        return ans;
+
+    // right to left
+    for (int i = n - 2; i >= 0; --i)
     {
-        if (n <= 0)
-        {
-            return false;
-        }
-        for (auto x : {2, 3, 5})
-        {
-            n = helper(n, x);
-        }
-        return n == 1;
+        while (size(ans) > 1 && orientation(ans[size(ans) - 2], ans.back(), points[i]) < 0)
+            ans.pop_back();
+        ans.push_back(points[i]);
     }
-};
+    ans.pop_back();
+    return ans;
+}
+
+static int orientation(const vector<int> &a, const vector<int> &b, const vector<int> &c)
+{
+    return (b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0]);
+}
 void solve()
 {
 }
