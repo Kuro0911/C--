b01caa4d897201d3c12d2c704c16eb6bde647953
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-02 10:54:05 +0530

    init

--- network/clientTEST.py
+++ network/clientTEST.py
@@ -1,13 +1,24 @@
 import socket
 import time
 
-c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
 port = 9999
 
 c.connect((host, port))
 
-msg = time.ctime(time.time())
-c.sendto(msg.encode(), (host, port))
+print(c.recv(1024).decode() + "server time")
+c.send(time.ctime(time.time()).encode())
+
+while True:
+    msg = input()
+    c.send(msg.encode())
+    if msg == 'quit':
+        break
+    data = c.recv(1024).decode()
+    if(data == 'quit'):
+        print("server closed the conn")
+        break
+    print(data)
 c.close()
--- network/serverTEST.py
+++ network/serverTEST.py
@@ -1,14 +1,30 @@
 import socket
 import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
 port = 9999
 
 s.bind((host, port))
 
+s.listen(5)
+
+conn, addr = s.accept()
+
+conn.send(time.ctime(time.time()).encode())
+print(conn.recv(1024).decode() + "client time")
+
 while True:
-    print(s.recvfrom(1024)[0].decode())
-    break
+    data = conn.recv(1024).decode()
+    if(data == 'quit'):
+        print("client closed the conn")
+        conn.close()
+        break
+    print(data)
+    msg = input()
+    conn.send(msg.encode())
+    if msg == 'quit':
+        conn.close()
+        break
 s.close()
