4e284f4fd54e29c3dabe476e3d1bd8d087fc3563
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-14 11:54:27 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -43,32 +43,9 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 }
 
 // #####################################################
-class Solution
-{
-public:
-    int climbStairs(int n)
-    {
-        if (n == 1)
-        {
-            return 1;
-        }
-        if (n == 2)
-        {
-            return 2;
-        }
-        vector<int> dp(n, 0);
-        dp[0] = 1;
-        dp[1] = 2;
-        for (int i = 2; i < n; i++)
-        {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n - 1];
-    }
-};
+
 void solve()
 {
-    string x = "aaa";
 }
 
 signed main()
