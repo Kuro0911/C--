0dd31114ee5db41156325da53e63a237c184efb8
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-30 00:00:34 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -43,53 +43,11 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 }
 
 // #####################################################
-class Solution
-{
-public:
-    vector<int> getOrder(vector<vector<int>> &t)
-    {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        vector<vector<int>> tasks;
-        for (int i = 0; i < t.size(); i++)
-        {
-            tasks.push_back({t[i][0], t[i][1], i});
-        }
-        sort(tasks.begin(), tasks.end());
-        long long currTime = 0;
-        int i = 0;
-        vector<int> res;
 
-        while (i < t.size() or !pq.empty())
-        {
-            if (pq.empty() and currTime < tasks[i][0])
-            {
-                currTime = tasks[i][0];
-            }
-            while (i < tasks.size() and currTime >= tasks[i][0])
-            {
-                pq.push({tasks[i][1], tasks[i][2]});
-                i++;
-            }
-
-            currTime += pq.top().first;
-            res.push_back(pq.top().second);
-            pq.pop();
-        }
-        return res;
-    }
-};
 void solve()
 {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    for (int i = 0; i < 10; i++)
-    {
-        pq.push({i, -i});
-    }
-    while (!pq.empty())
-    {
-        cout << pq.top();
-        pq.pop();
-    }
+    string x(10, ' ');
+    cout << x;
 }
 
 signed main()
