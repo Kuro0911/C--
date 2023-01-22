184562ec2fc75e9e01afab2968be6938770f90e7
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-01-22 09:47:57 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -43,58 +43,14 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 }
 
 // #####################################################
-class Solution
-{
-public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>> &edges)
-    {
-        queue<int> q;
-        vector<vector<int>> graph(n);
-        vector<int> vis(n, false);
-        vector<int> inDeg(n);
-        for (auto x : edges)
-        {
-            graph[x[0]].push_back(x[1]);
-            graph[x[1]].push_back(x[0]);
-            inDeg[x[0]]++;
-            inDeg[x[1]]++;
-        }
-        for (auto x : inDeg)
-        {
-            if (x == 1)
-            {
-                q.push(x);
-            }
-        }
-        vector<int> top;
-        while (!q.empty())
-        {
-            int sz = q.size();
-            for (int i = 0; i < sz; i++)
-            {
-                int temp = q.front();
-                q.pop();
-                if (!vis[temp])
-                {
-                    inDeg[temp]--;
-                    if (inDeg[temp] == 1)
-                    {
-                        q.push(temp);
-                    }
-                    top.push_back(temp);
-                    vis[temp] = true;
-                }
-            }
-        }
-        for (auto x : top)
-        {
-            cout << x << " ";
-        }
-        return {};
-    }
-};
+
 void solve()
 {
+    string s = "abc";
+    for (int i = 1; i <= s.size(); i++)
+    {
+        cout << s.substr(0, i) << " " << s.substr(i) << endl;
+    }
 }
 
 signed main()
