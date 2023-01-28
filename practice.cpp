f8586e9159274f1fbd934823c61d8662f3e942b1
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-01-29 01:42:34 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -6,12 +6,12 @@ using namespace std;
 #define all(a) (a).begin(), (a).end()
 #define sz(x) ((int)x.size())
 #define MOD (int)(1e9 + 7)
-typedef pair<int, int> pii;
-typedef pair<pii, int> ppi;
-typedef pair<int, pii> pip;
-typedef pair<pii, pii> ppp;
+typedef pair <int, int> pii;
+typedef pair <pii, int> ppi;
+typedef pair <int, pii> pip;
+typedef pair <pii, pii> ppp;
 
-// #####################################################
+//#####################################################
 
 template <typename T>
 ostream &operator<<(ostream &os, const vector<T> &v)
@@ -42,77 +42,32 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
     return os;
 }
 
-// #####################################################
-class SummaryRanges
-{
-public:
-    vector<int> vec;
-    SummaryRanges() {}
+//#####################################################
 
-    void addNum(int value)
-    {
-        vec.push_back(value);
-    }
-
-    vector<vector<int>> getIntervals()
-    {
-        vector<vector<int>> res;
-        sort(vec.begin(), vec.end());
-        for (int i = 0; i < vec.size(); i++)
-        {
-            if (res.size() == 0)
-            {
-                res.push_back({vec[i], vec[i]});
-                continue;
-            }
-            int ptr = res.size() - 1;
-            if (res[ptr][1] == vec[i])
-            {
-                continue;
-            }
-            if (res[ptr][1] + 1 == vec[i])
-            {
-                res[ptr][1] = vec[i];
-            }
-            else
-            {
-                res.push_back({vec[i], vec[i]});
-            }
-        }
-        return res;
-    }
-};
-
-/**
- * Your SummaryRanges object will be instantiated and called as such:
- * SummaryRanges* obj = new SummaryRanges();
- * obj->addNum(value);
- * vector<vector<int>> param_2 = obj->getIntervals();
- */
-void solve()
-{
+void solve(){
+    
 }
 
-signed main()
-{
+
+signed main(){
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     int t = 1;
-    // cin>>t;
+    //cin>>t;
     while (t--)
     {
         solve();
     }
-// for (int i = 1; i <= t; i++)
-//{
-// cout << "Case #" << i << ": " ;
-// solve();
-//}
-#ifndef ONLINE_JUDGE
-    cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
-#endif
-    return 0;
+    //for (int i = 1; i <= t; i++)
+    //{
+    //cout << "Case #" << i << ": " ;
+        //solve();
+    //}
+    #ifndef ONLINE_JUDGE
+    cerr <<"Time :"<<1000*((double)clock()) / (double)CLOCKS_PER_SEC<<"ms";
+    #endif
+        return 0;
 }
