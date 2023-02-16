27be2086340846025f2f5e1383a8a3a62eaec5bc
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-02-16 22:23:35 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -1,48 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-#define int long long
-#define tb ' '
-#define all(a) (a).begin(), (a).end()
-#define sz(x) ((int)x.size())
-#define MOD (int)(1e9 + 7)
-typedef pair<int, int> pii;
-typedef pair<pii, int> ppi;
-typedef pair<int, pii> pip;
-typedef pair<pii, pii> ppp;
-
-// #####################################################
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &v)
-{
-    for (auto &x : v)
-        os << x << " ";
-    os << endl;
-    return os;
-}
-template <typename T>
-ostream &operator<<(ostream &os, const set<T> &v)
-{
-    for (auto it : v)
-        os << it << " ";
-    return os;
-}
-template <typename T, typename S>
-ostream &operator<<(ostream &os, const map<T, S> &v)
-{
-    for (auto it : v)
-        os << it.first << " : " << it.second << endl;
-    return os;
-}
-template <typename T, typename S>
-ostream &operator<<(ostream &os, const pair<T, S> &v)
-{
-    os << v.first << " : " << v.second << endl;
-    return os;
-}
-
-// #####################################################
 
 class Complex
 {
@@ -83,7 +40,7 @@ Complex sum(Complex n1, Complex n2)
     return n3;
 }
 
-void solve()
+int main()
 {
     Complex n1, n2;
     n1.set_num(12, 2);
@@ -95,27 +52,5 @@ void solve()
     // using class
     Sum x;
     x.sum(n1, n2);
-}
-signed main()
-{
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t = 1;
-    // cin>>t;
-    while (t--)
-    {
-        solve();
-    }
-// for (int i = 1; i <= t; i++)
-//{
-// cout << "Case #" << i << ": " ;
-// solve();
-//}
-#ifndef ONLINE_JUDGE
-    cerr << "Time :" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
-#endif
     return 0;
 }
