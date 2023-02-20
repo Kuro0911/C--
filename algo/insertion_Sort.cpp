33987c13cf2c41965a9804dea60ebfde6ae13a4d
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-02-21 02:14:51 +0530

    init

--- algo/insertion_Sort.cpp
+++ algo/insertion_Sort.cpp
@@ -1,4 +1,4 @@
-//insersion sort = INSERTS NEXT ELE TO A ALREDY SORTED ARR BY USING PAIRWISE SWAPS ;
+// insersion sort = INSERTS NEXT ELE TO A ALREDY SORTED ARR BY USING PAIRWISE SWAPS ;
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -10,26 +10,31 @@ void printarr(int *a, int n)
     }
     cout << endl;
 };
-void insersion_sort(int * a , int n){
-        int key , j;
-    for (int i = 1; i <= n-1; i++)
+void insersion_sort(int *a, int n)
+{
+    int key, j;
+    int cnt = 0;
+    for (int i = 1; i <= n - 1; i++)
     {
-        //loop for each pass;
+        // loop for each pass;
         key = a[i];
         j = i - 1;
-        while (j>=0 && a[j] > key)
+        while (j >= 0 && a[j] > key)
         {
-            a[j+1] = a[j];
+            a[j + 1] = a[j];
+            cnt++;
             j--;
         }
-        a[j+1] = key;
+        a[j + 1] = key;
     }
+    cout << cnt << endl;
 };
-int main(){
-    int a[] = {10, 45, 21, 8, 50,78};
+int main()
+{
+    int a[] = {6, 8, 19, 48, 9, 90};
     int n = 6;
-    printarr(a , n);
-    insersion_sort(a , n);
-    printarr(a , n);
+    printarr(a, n);
+    insersion_sort(a, n);
+    printarr(a, n);
     return 0;
 }
