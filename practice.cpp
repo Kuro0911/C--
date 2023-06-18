30d42dc74b74fb5f3f5b10e9d07752c2abb7e970
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-06-18 14:04:38 +0530

    init

--- practice.cpp
+++ practice.cpp
@@ -46,43 +46,6 @@ ostream &operator<<(ostream &os, const pair<T, S> &v)
 
 void solve()
 {
-    vector<vector<char>> graph(10, vector<char>(10));
-    pair<int, int> root;
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            cin >> graph[i][j];
-            if (graph[i][j] == 'L')
-                root = {i, j};
-        }
-    }
-
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-    pq.push({0, root});
-    vector<pair<int, int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    while (!pq.empty())
-    {
-        auto temp = pq.top();
-        int dist = temp.first;
-        auto curr = temp.second;
-        int i = curr.first, j = curr.second;
-        pq.pop();
-        if (graph[i][j] == 'B')
-        {
-            cout << dist - 1;
-            return;
-        }
-        graph[i][j] = '#';
-        for (auto d : dir)
-        {
-            int new_x = i + d.first, new_y = j + d.second;
-            if (new_x >= 0 and new_y >= 0 and new_x < 10 and new_y < 10 and graph[new_x][new_y] != '#' and graph[new_x][new_y] != 'R')
-            {
-                pq.push({dist + 1, {new_x, new_y}});
-            }
-        }
-    }
 }
 
 signed main()
@@ -92,8 +55,8 @@ signed main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    freopen("buckets.in", "r", stdin);
-    freopen("buckets.out", "w", stdout);
+    // freopen("mixmilk.in", "r", stdin);
+    // freopen("mixmilk.out", "w", stdout);
 
     int t = 1;
     // cin >> t;
